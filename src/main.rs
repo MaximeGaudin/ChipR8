32591bdba06c9d566cc31133e7266d91d7b42eb7
Maxime Gaudin <me@maxime.ly>
2026-03-06 15:57:18 +0100

    Add rom name in parameters

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{env, error::Error};
 
 mod instructions;
 use instructions::base::Instruction;
@@ -19,13 +19,19 @@ fn fetch_decode_execute(vm: &mut vm::VM) {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+
     let mut vm = vm::init(vm::EmulationMode::Chip8);
     let mut raylib_context = screen::init();
     let opcode_per_frame = vm::CPU_TICK_RATE / screen::FRAME_RATE;
 
-    vm::load_rom("roms/maze.ch8".to_string(), &mut vm).unwrap();
+    let rom_path = if args.len() > 1 {
+        format!("roms/{}.ch8", &args[1])
+    } else {
+        "roms/corax+.ch8".to_string()
+    };
+    vm::load_rom(rom_path, &mut vm).unwrap();
 
-    // Boucle principale
     while !raylib_context.handle.window_should_close() {
         for _ in 0..opcode_per_frame {
             fetch_decode_execute(&mut vm);
